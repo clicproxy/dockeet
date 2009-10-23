6bb7e407ddf08dd3fe4ca64f02473d593f93b983
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-10-23 18:06:51 +0000

    symfony

---
+++ trunk/config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/home/willou/public_html/symfony/1.3/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
