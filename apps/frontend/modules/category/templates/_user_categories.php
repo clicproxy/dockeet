05063ddc8e52fa34e28675b7ed221df526d55dfb
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-05 03:25:28 +0000

    Premières lignes pour pouvoir ajouter un/des utilisateurs à une catégories.

---
+++ apps/frontend/modules/category/templates/_user_categories.php
@@ -0,0 +1,23 @@
+<h3><?php echo __('Users'); ?></h3>
+
+<?php include_partial('navigation/notice'); ?>
+
+
+<?php if (0 < count($form->getObject()->Users)): ?>
+  <ul id="category_users">
+    <?php foreach($form->getObject()->Users as $user):?>
+      <li id="category_<?php echo $user->id; ?>">
+        <?php echo $user->username; ?>
+        <a href="<?php echo url_for('document/removeUser?slug=' . $form->getObject()->slug . '&user_id=' . $->id); ?>" onclick="if (confirm('<?php echo __('Are you sure ?'); ?>')) categoryCtrl.removeUser(this); return false;">
+          <?php echo __('remove'); ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+
+<em><?php echo __('Add another user'); ?></em>
+<form action="<?php echo url_for('document/addUser'); ?>" method="post" onsubmit="categoryCtrl.addUser(this); return false;">
+  <?php echo $form; ?>
+  <input type="submit" value="<?php echo __('Add'); ?>">
+</form>
