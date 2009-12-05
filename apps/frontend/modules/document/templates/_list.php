44283419a5d5777868f005330d2f77408c668503
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-05 18:31:10 +0000

    Pagination des documents

--- apps/frontend/modules/document/templates/_list.php
+++ apps/frontend/modules/document/templates/_list.php
@@ -11,6 +11,48 @@
     <a href="<?php echo url_for('document/index?slug=' . $document->slug); ?>"><?php echo $document->title; ?></a>
   <?php endforeach; ?>
   
-  <?php echo $pager->count() . ' ' . ((1 < $pager->count()) ? __('documents') : __('document')) . '.'; ?>
-  <?php // TODO: pagination; ?>
+  <span><?php echo $pager->count() . ' ' . ((1 < $pager->count()) ? __('documents') : __('document')) . '.'; ?></span>
+  
+  <?php if ($pager->haveToPaginate()): ?>
+  <ul>
+  
+    <li>
+      <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=1'); ?>">
+        <span><?php echo __('First'); ?></span>
+      </a>
+    </li>
+
+    <li>
+      <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getPreviousPage()); ?>">
+        <span><?php echo __('Previous'); ?></span>
+      </a>
+    </li>
+
+    <?php foreach ($pager->getLinks() as $page): ?>
+    <li>
+      <?php if ($page == $pager->getPage()): ?>
+        <span><?php echo $page; ?></span>
+      <?php else: ?>
+        <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $page); ?>">
+          <span><?php echo $page; ?></span>
+        </a>
+      <?php endif; ?>
+    </li>
+    <?php endforeach; ?>
+
+    <li>
+      <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getNextPage()); ?>">
+        <span><?php echo __('Next'); ?></span>
+      </a>
+    </li>
+
+    <li>
+      <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getLastPage()); ?>">
+        <span><?php echo __('Last'); ?></span>
+      </a>
+    </li>
+
+  </ul>
+  <?php endif; ?>
+  
 </div>
