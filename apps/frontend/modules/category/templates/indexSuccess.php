31779192392b14f307ec6813087f1c589178077b
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-05 15:02:33 +0000

    Droits utilisateurs : categories & nombres de documents dans les catégories.

--- apps/frontend/modules/category/templates/indexSuccess.php
+++ apps/frontend/modules/category/templates/indexSuccess.php
@@ -1,4 +1,4 @@
-<h2><?php echo $title; ?></h2>
+<h2><?php echo (!is_null($category) && $category->getRawValue() instanceof Category) ? __('Category') . ' ' . $category->title : __('Homepage'); ?></h2>
 
 
 <?php if (null !==  $category && $category->getRawValue() instanceof Category): ?>
