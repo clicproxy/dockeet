e0f6e9c4789bcc57ae468dc85909ccb8a2300f51
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-06 23:22:25 +0000

    Design

--- trunk/apps/frontend/modules/category/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/category/templates/indexSuccess.php
@@ -41,7 +41,7 @@
 </div>
 
 <?php if ($sf_user->isAuthenticated() && null !==  $category && $category->getRawValue() instanceof Category): ?>
-  <div id="category_control_box">
+  <div id="category_control_box" class="control_box">
     <ul>
       <?php if ($sf_user->hasSubscribed($category->getRawValue())): ?>
         <li><a href="<?php echo url_for('category/unsubscribe?slug=' . $category->slug); ?>"><?php echo __('Unsubscribe'); ?></a></li>
@@ -55,30 +55,4 @@
   </div>
 <?php endif; ?>
 
-
-<div id="document_box">
-  <?php foreach ($pager->getResults() as $document): ?>
-    <?php include_partial('document/view_' . $sf_user->getAttribute('display', 'thumbnail', 'frontend'), array('document' => $document)); ?>
-  <?php endforeach; ?>
-  
-  <span class="pager_count"><?php echo $pager->count() . ' ' . ((1 < $pager->count()) ? __('documents') : __('document')) . '.'; ?></span>
-  
-  <?php if ($pager->haveToPaginate()): ?>
-    <ul class="pager">
-    
-      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=1'); ?>"><span><?php echo __('First'); ?></span></a></li>
-      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getPreviousPage()); ?>"><span><?php echo __('Previous'); ?></span></a></li>
-      <?php foreach ($pager->getLinks() as $page): ?>
-      <li>
-        <?php if ($page == $pager->getPage()): ?>
-          <span><?php echo $page; ?></span>
-        <?php else: ?>
-          <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $page); ?>"><span><?php echo $page; ?></span></a>
-        <?php endif; ?>
-      </li>
-      <?php endforeach; ?>
-      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getNextPage()); ?>">        <span><?php echo __('Next'); ?></span></a></li>
-      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getLastPage()); ?>"><span><?php echo __('Last'); ?></span></a></li>
-    </ul>
-  <?php endif; ?>
-</div>
+<?php include_partial('document/list', array('pager' => $pager)); ?>
---
+++ trunk/apps/frontend/modules/document/templates/_list.php
@@ -0,0 +1,26 @@
+<div id="document_box">
+  <?php foreach ($pager->getResults() as $document): ?>
+    <?php include_partial('document/view_' . $sf_user->getAttribute('display', 'thumbnail', 'frontend'), array('document' => $document)); ?>
+  <?php endforeach; ?>
+  
+  <span class="pager_count"><?php echo $pager->count() . ' ' . ((1 < $pager->count()) ? __('documents') : __('document')) . '.'; ?></span>
+  
+  <?php if ($pager->haveToPaginate()): ?>
+    <ul class="pager">
+    
+      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=1'); ?>"><span><?php echo __('First'); ?></span></a></li>
+      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getPreviousPage()); ?>"><span><?php echo __('Previous'); ?></span></a></li>
+      <?php foreach ($pager->getLinks() as $page): ?>
+      <li>
+        <?php if ($page == $pager->getPage()): ?>
+          <span><?php echo $page; ?></span>
+        <?php else: ?>
+          <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $page); ?>"><span><?php echo $page; ?></span></a>
+        <?php endif; ?>
+      </li>
+      <?php endforeach; ?>
+      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getNextPage()); ?>">        <span><?php echo __('Next'); ?></span></a></li>
+      <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getLastPage()); ?>"><span><?php echo __('Last'); ?></span></a></li>
+    </ul>
+  <?php endif; ?>
+</div>
