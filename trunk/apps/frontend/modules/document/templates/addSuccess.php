064a583e40d9827f6d0f4e6dfe5050794f970bd9
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-06 23:40:58 +0000

    Design

--- trunk/apps/frontend/modules/document/templates/addSuccess.php
+++ trunk/apps/frontend/modules/document/templates/addSuccess.php
@@ -1,7 +1,22 @@
-<h2><?php echo __('Add a document'); ?></h2>
+<div id="title_box">
+  <div id="title_left"></div>
+  <div id="title_content">
+    <h2><?php echo __('Add a document'); ?></h2>
+  </div>
+  <div id="title_right"></div>
+</div>
+
 
 
 <form method="post" enctype="multipart/form-data">
-  <?php echo $form; ?>
-  <input type="submit" value="<?php echo __('Save'); ?>" />
+  <?php echo $form->renderHiddenFields(); ?>
+  <?php echo $form->renderGlobalErrors(); ?>
+  
+  <?php echo $form['file']->renderError(); ?>
+  <?php echo $form['file']->renderLabel(); ?>
+  <?php echo $form['file']; ?>
+  <?php echo $form['document_category']['category_id']->renderError(); ?>
+  <?php echo $form['document_category']['category_id']->renderLabel(); ?>
+  <?php echo $form['document_category']['category_id']; ?>
+  <input class="submit" type="submit" value="<?php echo __('Save'); ?>" />
 </form>
