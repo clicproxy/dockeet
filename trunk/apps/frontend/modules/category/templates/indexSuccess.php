eb861baa70632b8fb321b6fb93d195ed4cbcc50c
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-06 22:10:37 +0000

    3 display

--- trunk/apps/frontend/modules/category/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/category/templates/indexSuccess.php
@@ -1,4 +1,44 @@
-<h2><?php echo (!is_null($category) && $category->getRawValue() instanceof Category) ? __('Category') . ' ' . $category->title : __('Homepage'); ?></h2>
+<div id="title_box">
+  <div id="title_left"></div>
+  <div id="title_content">
+    <h2><?php echo (!is_null($category) && $category->getRawValue() instanceof Category) ? __('Category') . ' ' . $category->title : __('Homepage'); ?></h2>
+    
+    <ul id="document_view_box">
+      <li>
+        <a id="display_thumbnail" href="<?php echo url_for('navigation/setDisplay?display=thumbnail'); ?>" <?php if ('thumbnail' === $sf_user->getAttribute('display', 'thumbnail', 'frontend')): ?>class="current"<?php endif;?>>
+          <span><?php echo __("Thumbnail"); ?></span>
+        </a>
+      </li>
+      <li>
+        <a id="display_mixed" href="<?php echo url_for('navigation/setDisplay?display=mixed'); ?>" <?php if ('mixed' === $sf_user->getAttribute('display', 'thumbnail', 'frontend')): ?>class="current"<?php endif;?>>
+          <span><?php echo __("Mixed"); ?></span>
+        </a>
+      </li>
+      <li>
+        <a id="display_detail" href="<?php echo url_for('navigation/setDisplay?display=detail'); ?>" <?php if ('detail' === $sf_user->getAttribute('display', 'thumbnail', 'frontend')): ?>class="current"<?php endif;?>>
+          <span><?php echo __("Detail"); ?></span>
+        </a>
+      </li>
+    </ul>
+    
+    <div id="sort_box">
+      <form action="<?php echo url_for('navigation/setOrder'); ?>" method="get">
+        <select onchange="jQuery(jQuery(this).parent()).submit(); return false;" name="order_by">
+          <option value="updated_at" <?php echo 0 === strpos($sf_user->getAttribute('order_by', 'updated_at DESC', 'frontend'), 'updated_at') ? 'selected="selected"' : ''; ?>>
+            <?php echo __("Date"); ?>
+          </option>
+          <option value="title" <?php echo 0 === strpos($sf_user->getAttribute('order_by', 'updated_at DESC', 'frontend'), 'title') ? 'selected="selected"' : ''; ?>>
+          <?php echo __("Alpha"); ?>
+          </option>
+          <option value="size" <?php echo 0 === strpos($sf_user->getAttribute('order_by', 'updated_at DESC', 'frontend'), 'size') ? 'selected="selected"' : ''; ?>>
+          <?php echo __("Size"); ?>
+          </option>
+        </select>
+      </form>
+    </div>
+  </div>
+  <div id="title_right"></div>
+</div>
 
 <?php if ($sf_user->isAuthenticated() && null !==  $category && $category->getRawValue() instanceof Category): ?>
   <div id="category_control_box">
@@ -15,39 +55,16 @@
   </div>
 <?php endif; ?>
 
-<div id="document_view_box">
-  <ul>
-    <li><a href="<?php echo url_for('navigation/setDisplay?display=thumbnail'); ?>"><span><?php echo __("Thumbnail"); ?></span></a></li>
-    <li><a href="<?php echo url_for('navigation/setDisplay?display=mixed'); ?>"><span><?php echo __("Mixed"); ?></span></a></li>
-    <li><a href="<?php echo url_for('navigation/setDisplay?display=detail'); ?>"><span><?php echo __("Detail"); ?></span></a></li>
-  </ul>
-</div>
-
-<div id="sort_box">
-  <form action="<?php echo url_for('navigation/setOrder'); ?>" method="get">
-    <select onchange="jQuery(jQuery(this).parent()).submit(); return false;" name="order_by">
-      <option value="updated_at" <?php echo 0 === strpos($sf_user->getAttribute('order_by', 'updated_at DESC', 'frontend'), 'updated_at') ? 'selected="selected"' : ''; ?>>
-        <?php echo __("Date"); ?>
-      </option>
-      <option value="title" <?php echo 0 === strpos($sf_user->getAttribute('order_by', 'updated_at DESC', 'frontend'), 'title') ? 'selected="selected"' : ''; ?>>
-      <?php echo __("Alpha"); ?>
-      </option>
-      <option value="size" <?php echo 0 === strpos($sf_user->getAttribute('order_by', 'updated_at DESC', 'frontend'), 'size') ? 'selected="selected"' : ''; ?>>
-      <?php echo __("Size"); ?>
-      </option>
-    </select>
-  </form>
-</div>
 
 <div id="document_box">
   <?php foreach ($pager->getResults() as $document): ?>
     <?php include_partial('document/view_' . $sf_user->getAttribute('display', 'thumbnail', 'frontend'), array('document' => $document)); ?>
   <?php endforeach; ?>
   
-  <span><?php echo $pager->count() . ' ' . ((1 < $pager->count()) ? __('documents') : __('document')) . '.'; ?></span>
+  <span class="pager_count"><?php echo $pager->count() . ' ' . ((1 < $pager->count()) ? __('documents') : __('document')) . '.'; ?></span>
   
   <?php if ($pager->haveToPaginate()): ?>
-    <ul>
+    <ul class="pager">
     
       <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=1'); ?>"><span><?php echo __('First'); ?></span></a></li>
       <li><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getPreviousPage()); ?>"><span><?php echo __('Previous'); ?></span></a></li>
--- trunk/apps/frontend/modules/document/templates/_view_detail.php
+++ trunk/apps/frontend/modules/document/templates/_view_detail.php
@@ -2,5 +2,9 @@
   <a href="<?php echo url_for('document/index?slug=' . $document->slug); ?>">
     <?php echo $document->title; ?>
   </a>
-  <span><?php echo date('d/m/Y', strtotime($document->updated_at)); ?></span>
+  <ul>
+    <li><?php echo __("Last update"). ' : ' . date('d/m/Y', strtotime($document->updated_at)); ?></li>
+    <li><?php echo __("First upload"). ' : ' . date('d/m/Y', strtotime($document->created_at)); ?></li>
+    <li><?php echo __('Size') . ' : ' . number_format($document->size /1024 , 2) . ' ' . __("Ko"); ?></li>
+  </ul>
 </div>
