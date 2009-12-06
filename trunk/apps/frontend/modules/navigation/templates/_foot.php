ea976da7e4afc14c1c1082109659558ec92196ff
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2009-12-06 20:32:04 +0000

    Footer terminé

--- trunk/apps/frontend/modules/navigation/templates/_foot.php
+++ trunk/apps/frontend/modules/navigation/templates/_foot.php
@@ -1,4 +1,5 @@
 <div id="foot" class="column">
+  <hr/>
   <ul>
     <li><a href="#"><?php echo __("Legals mentions")?></a></li>
     <li>&copy; 2009</li>
--- trunk/apps/frontend/modules/navigation/templates/_left.php
+++ trunk/apps/frontend/modules/navigation/templates/_left.php
@@ -7,8 +7,8 @@
         <?php if ('0' === $category->count_documents && !$sf_user->hasCredential('admin')) continue;?>
   	    <li>
   	      <a href="<?php echo url_for('category/index?slug=' . $category->slug); ?>" <?php if (0 === $i): ?>class="first"<?php endif;?><?php if ($max_i === $i): ?>class="last"<?php endif;?>>
-    	      <?php echo $category->title; ?>
             <span><?php echo $category->count_documents; ?></span>
+    	      <?php echo $category->title; ?>
           </a>
   	      
         </li>
