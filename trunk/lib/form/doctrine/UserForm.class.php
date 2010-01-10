61c3c4ac43809bcee70c0af5cbcb00fac005f3c4
david <david@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-01-10 06:16:25 +0000

    Édition culture utilisateur

--- trunk/lib/form/doctrine/UserForm.class.php
+++ trunk/lib/form/doctrine/UserForm.class.php
@@ -12,9 +12,10 @@ class UserForm extends BaseUserForm
 {
   public function configure()
   {
-    $this->useFields(array('username', 'password', 'email'));
+    $this->useFields(array('username', 'password', 'email', 'culture'));
     $this->widgetSchema['password'] = new sfWidgetFormInputPassword();
     $this->widgetSchema['password_confirm'] = new sfWidgetFormInputPassword();
+    $this->widgetSchema['culture'] = new sfWidgetFormI18nChoiceLanguage(array('add_emtpty' => true));
 
     $this->validatorSchema['password'] = new sfValidatorString(array('max_length' => 255, 'required' => !isset($this->getObject()->password)));
     $this->validatorSchema['password_confirm'] = new sfValidatorString(array('max_length' => 255, 'required' => !isset($this->getObject()->password)));
