2cb3bbb8e17a05581d6a930da0de7203859373dd
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-03-31 15:06:24 +0000

    Édition avec le titre en cachant le path.

--- lib/form/doctrine/BaseFormDoctrine.class.php
+++ lib/form/doctrine/BaseFormDoctrine.class.php
@@ -12,7 +12,5 @@ abstract class BaseFormDoctrine extends sfFormDoctrine
 {
   public function setup()
   {
-  	unset($this->widgetSchema['created_at'], $this->validatorSchema['created_at']);
-  	unset($this->widgetSchema['updated_at'], $this->validatorSchema['updated_at']);
   }
 }
--- lib/form/doctrine/CategoryFrontendForm.class.php
+++ lib/form/doctrine/CategoryFrontendForm.class.php
@@ -10,8 +10,43 @@
  */
 class CategoryFrontendForm extends CategoryForm
 {
+	/**
+	 * Configure les widgets/validators
+	 */
   public function configure()
   {
   	$this->useFields(array('title', 'description'));
+  	$this->setWidget('path', new sfWidgetFormInputHidden());
+  	$this->setValidator('path', new sfValidatorString(array('required' => false)));
   }
+
+  /**
+   * Set defaults
+   * @param mixed $defaults
+   */
+  public function setDefaults($defaults)
+  {
+    if (array() !== $defaults && !isset($defaults['path']))
+    {
+      $defaults['path'] = substr($defaults['title'], 0, strrpos($defaults['title'], '|')+1);
+      $defaults['title'] = substr($defaults['title'], strrpos($defaults['title'], '|')+1);
+      sfContext::getInstance()->getLogger()->info(var_export($defaults, true));
+    }
+
+  	return parent::setDefaults($defaults);
+  }
+
+  /**
+   * Concat path and title
+   * @param unknown_type $con
+   */
+  public function save($con = null)
+  {
+  	$this->values['title'] = $this->values['path'] . $this->values['title'];
+  	return parent::save($con);
+  }
+
+
+
+
 }
