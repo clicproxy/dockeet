0b2435e47360975627687e08eddc6566b11271dd
dockeetproject@gmail.com <dockeetproject@gmail.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-03-31 13:23:25 +0000

    style footer, navigation...

--- apps/frontend/modules/navigation/templates/_foot.php
+++ apps/frontend/modules/navigation/templates/_foot.php
@@ -1,10 +1,9 @@
 <div id="foot" class="column">
-  <hr/>
   <ul>
     <?php if ($sf_user->hasCredential('admin')):?>
       <li><a href="<?php echo url_for('services/index'); ?>"><?php echo __("Services")?></a></li>
     <?php endif; ?>
     <li><a href="#"><?php echo __("Legals mentions")?></a></li>
-    <li>&copy; 2009</li>
+    <li><span>&copy; 2010</span></li>
   </ul>
 </div>
