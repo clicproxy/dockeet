9e67feeac9be7f98e5bbc0ced329e048040d16b3
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-03-31 16:18:32 +0000

    Ne rempli par automatiquement les champs des mot de passes à partir des mémoires des navigateurs.

--- trunk/apps/frontend/modules/user/templates/editSuccess.php
+++ trunk/apps/frontend/modules/user/templates/editSuccess.php
@@ -9,30 +9,30 @@
 <form action="<?php echo url_for('user/edit'); ?>" method="post">
   <?php echo $form->renderHiddenFields(); ?>
   <?php echo $form->renderGlobalErrors(); ?>
-  
+
   <?php echo $form['username']->renderError(); ?>
   <?php echo $form['username']->renderLabel(); ?>
   <?php echo $form['username']; ?>
-  
+
   <?php echo $form['password']->renderError(); ?>
   <?php echo $form['password']->renderLabel(); ?>
-  <?php echo $form['password']; ?>
-  
+  <?php echo $form['password']->render(array('autocomplete' => 'off')); ?>
+
   <?php echo $form['password_confirm']->renderError(); ?>
   <?php echo $form['password_confirm']->renderLabel(); ?>
-  <?php echo $form['password_confirm']; ?>
-  
+  <?php echo $form['password_confirm']->render(array('autocomplete' => 'off')); ?>
+
   <?php echo $form['email']->renderError(); ?>
   <?php echo $form['email']->renderLabel(); ?>
   <?php echo $form['email']; ?>
-  
+
   <?php echo $form['culture']->renderError(); ?>
   <?php echo $form['culture']->renderLabel(); ?>
   <?php echo $form['culture']; ?>
-  
+
   <?php echo $form['admin']->renderError(); ?>
   <?php echo $form['admin']->renderLabel(); ?>
   <?php echo $form['admin']; ?>
-  
+
   <input class="submit" type="submit" value="<?php echo __('Save'); ?>" />
 </form>
