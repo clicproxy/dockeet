e4d2121f7864a31cc3985de45b3be9e830bcf592
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-01 10:26:12 +0000

    Génération de Thumbnail au vol

--- trunk/lib/form/doctrine/UserCategoryAddForm.class.php
+++ trunk/lib/form/doctrine/UserCategoryAddForm.class.php
@@ -4,10 +4,10 @@ class UserCategoryAddForm extends CategoryForm
   public function configure()
   {
     $this->useFields();
-    
+
     $this->embedForm('user_category', $this->getUserCategoryForm());
   }
-  
+
   /**
    * Return a UserCategoryForm with a new UserCategory linked with the category
    * @return DocumentCategoryForm
@@ -15,8 +15,8 @@ class UserCategoryAddForm extends CategoryForm
   protected function getUserCategoryForm ()
   {
     $user_category = new UserCategory();
-    $user_category->Category = $this->getObject();
+    $user_category->category_id = $this->getObject()->id;
     return new UserCategoryForm($user_category);
-  } 
-  
+  }
+
 }
