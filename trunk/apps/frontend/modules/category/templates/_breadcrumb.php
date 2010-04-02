0937aa29cf52cbe3131d39309f17c0fe65000a77
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-02 12:01:32 +0000

    Correction breadcrumb

--- trunk/apps/frontend/modules/category/templates/_breadcrumb.php
+++ trunk/apps/frontend/modules/category/templates/_breadcrumb.php
@@ -1,7 +1,7 @@
 <div id="breadcrumb">
 	<ul>
 	  <li><a href="<?php echo url_for("@homepage"); ?>"><?php echo __('Home')?></a></li>
-    <?php if ($category->getRawValue() instanceof Category): ?>
+    <?php if (null !== $category && $category->getRawValue() instanceof Category): ?>
 		  <?php foreach ($category->getBreadcrumb() as $slug => $title): ?>
 		    <li><a href="<?php echo url_for('category/index?slug=' . $slug); ?>"><?php echo $title; ?></a></li>
 		  <?php endforeach; ?>
