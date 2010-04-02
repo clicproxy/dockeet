fb31bcf030c46d4431d5512f488dd106dfb8ed34
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-02 09:40:39 +0000

    Rajout d'une classe dans la réponse de l'upload

--- apps/frontend/modules/document/templates/_document_upload_fail.php
+++ apps/frontend/modules/document/templates/_document_upload_fail.php
@@ -1,2 +1,2 @@
-<span class="error"><?php echo __("An error occurred during the uploading of the file")?></span>
+<span class="upload_response error"><?php echo __("An error occurred during the uploading of the file")?></span>
 <pre><?php echo $form->renderGlobalErrors(); ?></pre>
