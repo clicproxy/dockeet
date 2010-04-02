168b9f4c4c60ebfd11737931557f2ca7a5b5f9fe
dockeetproject@gmail.com <dockeetproject@gmail.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-02 12:34:31 +0000

    style dockeet

--- trunk/apps/frontend/modules/document/templates/editSuccess.php
+++ trunk/apps/frontend/modules/document/templates/editSuccess.php
@@ -9,29 +9,30 @@
 
 <a href="<?php echo url_for('document/index?slug=' . $form->getObject()->slug); ?>">&larr; <?php echo __('Back to the document'); ?></a>
 
-<form method="post" enctype="multipart/form-data">
-  <?php echo $form->renderHiddenFields(); ?>
-  <?php echo $form->renderGlobalErrors(); ?>
-  
-  <?php echo $form['title']->renderError(); ?>
-  <?php echo $form['title']->renderLabel(); ?>
-  <?php echo $form['title']; ?>
-  
-  <?php echo $form['description']->renderError(); ?>
-  <?php echo $form['description']->renderLabel(); ?>
-  <?php echo $form['description']; ?>
-  
-  <?php echo $form['public']->renderError(); ?>
-  <?php echo $form['public']->renderLabel(); ?>
-  <?php echo $form['public']; ?>
-  
-  <?php echo $form['file']->renderError(); ?>
-  <?php echo $form['file']->renderLabel(); ?>
-  <?php echo $form['file']; ?>
-  
-  <input class="submit" type="submit" value="<?php echo __('Save'); ?>" />
-</form>
-
+<div id="doc_edit">
+	<form method="post" enctype="multipart/form-data">
+	  <?php echo $form->renderHiddenFields(); ?>
+	  <?php echo $form->renderGlobalErrors(); ?>
+	  
+	  <?php echo $form['title']->renderError(); ?>
+	  <?php echo $form['title']->renderLabel(); ?>
+	  <?php echo $form['title']; ?>
+	  
+	  <?php echo $form['description']->renderError(); ?>
+	  <?php echo $form['description']->renderLabel(); ?>
+	  <?php echo $form['description']; ?>
+	  
+	  <?php echo $form['public']->renderError(); ?>
+	  <?php echo $form['public']->renderLabel(); ?>
+	  <?php echo $form['public']; ?>
+	  
+	  <?php echo $form['file']->renderError(); ?>
+	  <?php echo $form['file']->renderLabel(); ?>
+	  <?php echo $form['file']; ?>
+	  
+	  <input class="submit" type="submit" value="<?php echo __('Save'); ?>" />
+	</form>
+</div>
 
 <div id="document_categories">
   <?php include_partial('document_categories', array('form' => new DocumentCategoryAddForm($form->getObject()))); ?>
