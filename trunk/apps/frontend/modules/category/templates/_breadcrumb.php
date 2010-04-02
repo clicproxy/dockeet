ef17b339fb2e0b104358b533531fd912237f922b
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-02 10:34:50 +0000

    Correction du fil d'ariane.

--- trunk/apps/frontend/modules/category/templates/_breadcrumb.php
+++ trunk/apps/frontend/modules/category/templates/_breadcrumb.php
@@ -1,8 +1,11 @@
 <div id="breadcrumb">
 	<ul>
 	  <li><a href="<?php echo url_for("@homepage"); ?>"><?php echo __('Home')?></a></li>
-	  <?php foreach ($category->getBreadcrumb() as $slug => $title): ?>
-	    <li><a href="<?php echo url_for('category/index?slug=' . $slug); ?>"><?php echo $title; ?></a></li>
-	  <?php endforeach; ?>
+    <?php if ($category->getRawValue() instanceof Category): ?>
+		  <?php foreach ($category->getBreadcrumb() as $slug => $title): ?>
+		    <li><a href="<?php echo url_for('category/index?slug=' . $slug); ?>"><?php echo $title; ?></a></li>
+		  <?php endforeach; ?>
+      <li class="breadcrumb_last"><?php echo $category->getPublicTitle(); ?></li>
+    <?php endif; ?>
 	</ul>
 </div>
