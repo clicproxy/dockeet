24005ab2daecb48a9c1f0e1bbb18de277c5eed0a
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-02 07:39:43 +0000

    Rajout d'une classe pour le CSS

--- apps/frontend/modules/document/templates/_list.php
+++ apps/frontend/modules/document/templates/_list.php
@@ -4,21 +4,23 @@
   <?php foreach ($pager->getResults() as $document): ?>
     <?php include_partial('document/view_' . $sf_user->getAttribute('display', 'thumbnail', 'frontend'), array('document' => $document)); ?>
   <?php endforeach; ?>
-  
+
   <?php if ($pager->haveToPaginate()): ?>
   	<div class="clear"></div>
     <ul class="pager">
-    
+
       <li class="pager_first"><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=1'); ?>"><span><?php echo __('First'); ?></span></a></li>
       <li class="pager_previous">&laquo; <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getPreviousPage()); ?>"><span><?php echo __('Previous'); ?></span></a></li>
       <?php foreach ($pager->getLinks() as $page): ?>
-      <li>
         <?php if ($page == $pager->getPage()): ?>
-          <span><?php echo $page; ?></span>
+	        <li class="pager_active">
+	          <span><?php echo $page; ?></span>
+	        </li>
         <?php else: ?>
-          <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $page); ?>"><span><?php echo $page; ?></span></a>
+	        <li>
+	          <a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $page); ?>"><span><?php echo $page; ?></span></a>
+	        </li>
         <?php endif; ?>
-      </li>
       <?php endforeach; ?>
       <li class="pager_next"><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getNextPage()); ?>">        <span><?php echo __('Next'); ?></span></a> &raquo;</li>
       <li class="pager_last"><a href="<?php echo url_for('category/index?' . (is_null($category) ?  '' : 'slug=' . $category->slug . '&') . 'page=' . $pager->getLastPage()); ?>"><span><?php echo __('Last'); ?></span></a></li>
