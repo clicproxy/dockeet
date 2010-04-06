8f1eb2625c48334bac2aa4206a841661b196af77
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-06 07:39:04 +0000

    Sélection de la catégorie (A terminer !)

--- apps/frontend/modules/document/templates/_document_categories.php
+++ apps/frontend/modules/document/templates/_document_categories.php
@@ -18,13 +18,35 @@
   <p><?php echo __("No category has this document")?></p>
 <?php endif; ?>
 <div class="clear"></div>
-<em><?php echo __('Add in another category:'); ?></em>
 <form action="<?php echo url_for('document/addCategory'); ?>" method="post" onsubmit="documentCtrl.addCategory(this); return false;">
   <?php echo $form->renderHiddenFields(); ?>
   <?php echo $form->renderGlobalErrors(); ?>
-  
+
   <?php echo $form['document_category']['category_id']->renderError(); ?>
-  <?php echo $form['document_category']['category_id']->renderLabel(); ?>
   <?php echo $form['document_category']['category_id']; ?>
-  <input class="submit" type="submit" value="<?php echo __('Add'); ?>">
+
+  <a onclick="jQuery('ul#add_category').toggle(); return false;" href="#"><span><?php echo __("Add a category") ?></span></a>
+
+    <ul id="add_category" style="display: none;">
+      <?php $max_i = count($sf_user->getCategories(true)) -1; ?>
+      <?php foreach ($sf_user->getCategories(true) as $i => $category): ?>
+        <?php if ('0' === $category->count_documents && !$sf_user->hasCredential('admin')) continue;?>
+        <li class="root">
+          <a href="<?php echo url_for('category/index?slug=' . $category->slug); ?>" <?php if (0 === $i): ?>class="first"<?php endif;?><?php if ($max_i === $i): ?>class="last"<?php endif;?>>
+            <?php echo $category->getPublicTitle(); ?>
+          </a>
+          <?php if (0 < Doctrine::getTable('Category')->createQuery('c')->where('c.title LIKE ?', $category->title . '|%')->count()): ?>
+            <ul>
+              <?php foreach (Doctrine::getTable('Category')->createQuery('c')->where('c.title LIKE ?', $category->title . '|%')->execute() as $sub_category): ?>
+                <li class="sub_category subcat_level_<?php echo substr_count($sub_category->title, '|'); ?>">
+                  <a href="<?php echo url_for("category/index?slug=" . $sub_category->slug); ?>"><?php echo $sub_category->getPublicTitle(); ?></a>
+                </li>
+              <?php endforeach;?>
+              <li class="foot_sub_category"></li>
+            </ul>
+          <?php endif; ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+
 </form>
