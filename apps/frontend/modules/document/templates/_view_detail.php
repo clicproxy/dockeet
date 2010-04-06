b537accc3bc24c87afca4a1ec1f010baeae46562
dockeetproject@gmail.com <dockeetproject@gmail.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-06 13:39:23 +0000

    style dockeet

--- apps/frontend/modules/document/templates/_view_detail.php
+++ apps/frontend/modules/document/templates/_view_detail.php
@@ -1,10 +1,10 @@
-<div class="document_detail">
+<div class="document_detail border_style">
   <a href="<?php echo url_for('document/index?slug=' . $document->slug); ?>">
     <?php echo $document->title; ?>
+	  <ul>
+	    <li class="updated"><?php echo __("Last update"). ' : '; ?> <strong><?php echo date('d/m/Y', strtotime($document->updated_at)); ?></strong></li>
+	    <li class="updated"><?php echo __("First upload"). ' : '; ?> <strong><?php echo date('d/m/Y', strtotime($document->created_at)); ?></strong></li>
+	    <li class="updated"><?php echo __('Size') . ' : '; ?> <strong><?php echo number_format($document->size /1024 , 2) . ' ' . __("Ko"); ?></strong></li>
+	  </ul>
   </a>
-  <ul>
-    <li><?php echo __("Last update"). ' : ' . date('d/m/Y', strtotime($document->updated_at)); ?></li>
-    <li><?php echo __("First upload"). ' : ' . date('d/m/Y', strtotime($document->created_at)); ?></li>
-    <li><?php echo __('Size') . ' : ' . number_format($document->size /1024 , 2) . ' ' . __("Ko"); ?></li>
-  </ul>
 </div>
