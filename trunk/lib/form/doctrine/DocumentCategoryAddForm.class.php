1aed3cc27a2bab816e4193aacf2e777f1d3a5cdf
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-06 16:12:42 +0000

    Ote les liens sur les catégories existantes.

--- trunk/lib/form/doctrine/DocumentCategoryAddForm.class.php
+++ trunk/lib/form/doctrine/DocumentCategoryAddForm.class.php
@@ -1,13 +1,13 @@
 <?php
-class DocumentCategoryAddForm extends DocumentForm 
+class DocumentCategoryAddForm extends DocumentForm
 {
   public function configure()
   {
     $this->useFields();
-    
+
     $this->embedForm('document_category', $this->getDocumentCategoryForm());
   }
-  
+
   /**
    * Return a DocumentCategoryForm with a new DocumentCategory linked with the document
    * @return DocumentCategoryForm
@@ -17,5 +17,5 @@ class DocumentCategoryAddForm extends DocumentForm
     $document_category = new DocumentCategory();
     $document_category->Document = $this->getObject();
     return new DocumentCategoryForm($document_category);
-  } 
-} 
+  }
+}
