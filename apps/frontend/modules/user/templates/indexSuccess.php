34612b375cf08b55a4ff2be41d8a1ef5a14d96d6
dockeetproject@gmail.com <dockeetproject@gmail.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-06 09:32:29 +0000

    correction bug boite slide sites pro

--- apps/frontend/modules/user/templates/indexSuccess.php
+++ apps/frontend/modules/user/templates/indexSuccess.php
@@ -6,20 +6,22 @@
   <div id="title_bottom"></div>
 </div>
 
-<table>
-  <thead>
-    <tr>
-      <th><?php echo __("Username"); ?></tH>
-      <th><?php echo __("Email"); ?></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach($pager->getResults() as $user): ?>
-      <tr>
-        <th><a href="<?php echo url_for('user/edit?username=' . $user->username); ?>"><?php echo $user->username; ?></a></th>
-        <td><?php echo $user->email; ?></td>
-        <td><a href="<?php echo url_for('user/delete?username=' . $user->username);?>" onclick="return confirm('<?php echo __('Are you sure ?'); ?>');"><?php echo __('delete'); ?></a></td>
-      </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<div id="">
+	<table>
+	  <thead>
+	    <tr>
+	      <th><?php echo __("Username"); ?></tH>
+	      <th><?php echo __("Email"); ?></th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <?php foreach($pager->getResults() as $user): ?>
+	      <tr>
+	        <th><a href="<?php echo url_for('user/edit?username=' . $user->username); ?>"><?php echo $user->username; ?></a></th>
+	        <td><?php echo $user->email; ?></td>
+	        <td><a href="<?php echo url_for('user/delete?username=' . $user->username);?>" onclick="return confirm('<?php echo __('Are you sure ?'); ?>');"><?php echo __('delete'); ?></a></td>
+	      </tr>
+	    <?php endforeach; ?>
+	  </tbody>
+	</table>
+</div>
