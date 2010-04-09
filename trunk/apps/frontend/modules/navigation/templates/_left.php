1e5246639bb1523df687faab611905da76a0449d
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-04-09 13:23:32 +0000

    Reste de trace de debug.

--- trunk/apps/frontend/modules/navigation/templates/_left.php
+++ trunk/apps/frontend/modules/navigation/templates/_left.php
@@ -20,7 +20,7 @@
 		          <?php foreach (Doctrine::getTable('Category')->createQuery('c')->where('c.title LIKE ?', $category->title . '|%')->execute() as $sub_category): ?>
                 <?php if (0 === $sub_category->countDocument(true, $sf_user->getUser()->getRawValue()) && !$sf_user->hasCredential('admin')) continue;?>
                 <li class="sub_category subcat_level_<?php echo substr_count($sub_category->title, '|'); ?>">
-                  <a class="border_stylehover" href="<?php echo url_for("category/index?slug=" . $sub_category->slug); ?>">&raquo; <?php echo $sub_category->getPublicTitle() . $sub_category->countDocument(true, $sf_user->getUser()->getRawValue()); ?> <span><?php echo $sub_category->countDocument(); ?></span></a>
+                  <a class="border_stylehover" href="<?php echo url_for("category/index?slug=" . $sub_category->slug); ?>">&raquo; <?php echo $sub_category->getPublicTitle(); ?> <span><?php echo $sub_category->countDocument(); ?></span></a>
                 </li>
 		          <?php endforeach;?>
               <li class="foot_sub_category"></li>
