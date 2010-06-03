3f2a2ab0a6c783cfdaf068316f8c95ac0c0978b4
david@bouchethomas.com <david@bouchethomas.com@8a1c91d4-1a52-11df-b1cb-2be540f5faec>
2010-06-03 10:09:53 +0000

    Administration des tags

--- trunk/web/js/document.js
+++ trunk/web/js/document.js
@@ -21,14 +21,34 @@ DocumentCtrl = function ()
     jQuery.get(jQuery(link).attr('href'), {}, function(data) { jQuery('div#document_categories').html(data) });
   };
   
+  /**
+   * Add a tag on a document
+   * @param link
+   * @return
+   */
   this.addTag = function (link)
   {
     jQuery.get(jQuery(link).attr('href'), {}, function(data) { jQuery('div#document_tags').html(data) });
   };
-  
+
+  /**
+   * Remove a tag from a document
+   * @param link
+   * @return
+   */
   this.removeTag = function (link)
   {
-    // TODO
+    jQuery.get(jQuery(link).attr('href'), {}, function(data) { jQuery('div#document_tags').html(data) });
+  };
+  
+  /**
+   * Add a new Tag on a document
+   * @param form
+   * @return
+   */
+  this.addNewTag = function (form)
+  {
+    jQuery.get(jQuery(form).attr('action') + "/tag/" + jQuery('input#new-tag').val(), jQuery(form).serialize(), function(data) { jQuery('div#document_tags').html(data) });
   };
 };
 
